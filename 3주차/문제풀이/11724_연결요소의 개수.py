1a2d1b4c193c6d08599688e2e0ba1c65bd6b7305
김지나 <jina4066@naver.com>
2025-05-17 01:30:57 +0900

    feat: 다른 방법으로 풀기

--- 3주차/문제풀이/11724_연결요소의 개수.py
+++ 3주차/문제풀이/11724_연결요소의 개수.py
@@ -1,26 +1,22 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+def dfs(v):
+    visited[v] = 1
+    for i in range(1, N+1):
+        if graph[v][i] == 1 and visited[i] == 0:
+            dfs(i)
 
-# dfs 함수
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+N, M = map(int, input().split())
 
-n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
-graph = [[] for _ in range(n+1)]
-for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+count = 0
+graph = [[0] * (N+1) for _ in range(N+1)]
+visited = [0] * (N+1)
 
-count = 0 # 연결 노드의 수
-visited = [False] * (n+1)
-for i in range(1, n+1):
-    if not visited[i]:
-        dfs(graph, i, visited)
-        count += 1 # dfs 한 번 끝날 때마다 count+1
+for i in range(M):
+    a, b = map(int, input(). split())
+    graph[a][b] = graph[b][a] = 1
+
+for i in range(1, N+1):
+    if visited[i] == 0:
+        dfs(i)
+        count += 1
 
 print(count)
