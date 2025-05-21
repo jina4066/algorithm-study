f218d0f9257b2db7731a07579529762eb44a8e52
김지나 <jina4066@naver.com>
2025-05-21 23:45:50 +0900

    feat: [4주차 3일차]

---
+++ 4주차/1463_1로 만들기.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+dp = [0] * ( N + 1)
+
+#dp[i] : 숫자 i를 만드는데 필요한 최소 연산 횟수
+for i in range (2, N+1) :
+    dp[i] = dp[i-1] + 1 
+    
+    if i % 2 == 0: # 2로 나누어 떨어지는 경우, 최소값 갱신
+        dp[i] = min(dp[i], dp[i//2] + 1) 
+    
+    if i % 3 == 0 :# 3으로 나누어 떨어지는 경우, 최소값 갱신
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[N])
